1b04a75eeefacde69dc430dcb00db1bc4a9086bd
Nassymple <100800574+Nassymple@users.noreply.github.com>
2023-07-19 16:42:24 +0000

    Update DBConnection.java

--- inventorySystem/inventorySystem/src/main/utils/DBConnection.java
+++ inventorySystem/inventorySystem/src/main/utils/DBConnection.java
@@ -10,7 +10,7 @@ public class DBConnection {
     // returns connection link
     public Connection getConnection(){
         // Specify connection params
-        String dbName = "dsa_db";
+        String dbName = "inventory";
         String url = "jdbc:mysql://localhost:3306/" + dbName;
         String username = "root";
         String password = "";
